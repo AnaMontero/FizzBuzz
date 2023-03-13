0fb8ef1005c11338b6b1fde05f731e83b25d2cbe
Ana Montero <anamongar@gmail.com>
2023-03-13 21:28:25 +0100

    feat: added logic to print Buzz when number is multiple of 5

--- app/src/main/java/fizzbuzz/kata/FizzBuzz.java
+++ app/src/main/java/fizzbuzz/kata/FizzBuzz.java
@@ -8,7 +8,7 @@ public class FizzBuzz {
         else if(number % 3 == 0){
             return "Fizz";
         }
-        else if(number == 5){
+        else if(number % 5 == 0){
             return "Buzz";
         }
 
