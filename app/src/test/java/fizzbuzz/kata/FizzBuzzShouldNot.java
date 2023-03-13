f74b8013349890e67dd657ccd11ccd3e9c2215dc
Ana Montero <anamongar@gmail.com>
2023-03-13 21:54:26 +0100

    test: verify return values are within limits

---
+++ app/src/test/java/fizzbuzz/kata/FizzBuzzShouldNot.java
@@ -0,0 +1,41 @@
+package fizzbuzz.kata;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class FizzBuzzShouldNot {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 5",
+            "31, Fizz",
+            "59, Buzz",
+            "99, FizzBuzz"
+    })
+    public void returnIncorrectValueWhenNumberIsBetWeenZeroAndOneHundred(int number, String expectedValue) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var actual = fizzBuzz.print(number);
+
+        assertNotEquals(expectedValue, actual);
+    }
+
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1, -1",
+            "101, 101",
+            "-3, Fizz",
+            "102, Fizz",
+            "-5, Buzz",
+            "110, Buzz",
+            "105, FizzBuzz"
+    })
+    public void returnValueWhenNumberIsNotBetWeenZeroAndOneHundred(int number, String expectedValue) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        var actual = fizzBuzz.print(number);
+
+        assertNotEquals(expectedValue, actual);
+    }
+}
