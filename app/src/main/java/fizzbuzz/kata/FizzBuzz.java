5cbb0786462aaccb82ca8cb12bdfebabc6e3ef9f
Ana Montero <anamongar@gmail.com>
2023-03-13 21:01:19 +0100

    refactor: refactored print method

--- app/src/main/java/fizzbuzz/kata/FizzBuzz.java
+++ app/src/main/java/fizzbuzz/kata/FizzBuzz.java
@@ -2,13 +2,10 @@ package fizzbuzz.kata;
 
 public class FizzBuzz {
     public String print(int number) {
-        var text = "";
         if(number == 3){
-            text = "Fizz";
+            return "Fizz";
         }
-        else {
-            text = Integer.toString(number);
-        }
-        return text;
+
+        return Integer.toString(number);
     }
 }
